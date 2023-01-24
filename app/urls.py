36cf4dc864a6d8a68585995673289db1b4f8b9ed
Eduardo <eduardotorres1980@gmail.com>
2023-01-24 00:06:05 -0300

    template producto

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
+from django import views
 from django.urls import path
-from .views import home, contacto, catalogo, nosotros
+from .views import euro, home, contacto, catalogo, nosotros, producto
 
 urlpatterns = [
     path('', home, name="home"),
     path('contacto', contacto, name="contacto"),
     path('catalogo', catalogo, name="catalogo"),
     path('nosotros', nosotros, name="nosotros"),
+    path('producto', producto, name="producto"),
+    path('euro', euro, name='euro'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+import requests
+
 # Create your views here.
 
 def home(request):
@@ -13,3 +15,10 @@ def catalogo(request):
 
 def nosotros(request):
     return render(request, 'app/nosotros.html')
+
+def producto(request):
+    return render(request, 'app/producto.html')
+
+def euro(request):
+    response=requests.get('https://api.cmfchile.cl/api-sbifv3/recursos_api/euro?apikey=fac1d9b01370e39370f86a5e9f2aa1a7081f4acf&formato=json').json
+    return render(request, 'app/euro.html', {'response':response})
